709f8e14ca1dbc9b971f1ba257789e428b8c45d6
MIdasCO <rusabi54@gmail.com>
2026-05-25 19:41:23 +0600

    Check

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = ("postgresql://admin:1234@db:5432/my_db")
+DATABASE_URL = ("postgresql://admin:1234@db:5438/my_db")
 engine = create_engine(DATABASE_URL)
 session_local = sessionmaker(autoflush=False, autocommit=False, bind=engine)
 
